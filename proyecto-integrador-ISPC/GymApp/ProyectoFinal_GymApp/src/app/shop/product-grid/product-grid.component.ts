ee294f9ce29173cd55e7af27537e8b48a95ee473
CarolinaEMedinaArg <carolinaestefaniamedina@gmail.com>
2023-05-22 01:00:58 -0300

    Fix css tienda

--- proyecto-integrador-ISPC/GymApp/ProyectoFinal_GymApp/src/app/shop/product-grid/product-grid.component.ts
+++ proyecto-integrador-ISPC/GymApp/ProyectoFinal_GymApp/src/app/shop/product-grid/product-grid.component.ts
@@ -18,6 +18,7 @@ export class ProductGridComponent implements OnInit {
     this.products = productService.getProducts();
     console.log('Products:', this.products);
   }
+  
   getItems() {
     return this.items;
   }
@@ -41,6 +42,7 @@ export class ProductGridComponent implements OnInit {
     console.log('Product selected:', product);}
     
   ngOnInit(): void {
+    
   }
 }
 
